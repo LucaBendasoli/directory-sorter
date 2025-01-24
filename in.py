a01463d4c34c9acdd80250941d6ae073d590143e
LucaAgostinho <lucaabenda@icloud.com>
2025-01-24 20:36:30 -0300

    chore: applying linting and good practices

--- in.py
+++ in.py
@@ -5,14 +5,20 @@ desktop into different folders categorized by their extensions."""
 
 import os
 
-extensions = []
-main_folder = False 
+def main():
+    """Sorts all the files in the same level directory
+into folders defined by the files extensions"""
 
-os.chdir(os.getcwd())
-for f in os.listdir():
-    if f.split(".")[-1] in ["py", "git", "gitattributes", "gitignore"]:
-        continue
-    if f.split(".")[-1] not in extensions:
-        extensions.append(f.split(".")[-1])
-        os.mkdir(f.split(".")[-1])
-    os.rename(f, f"{f.split(".")[-1]}\\{f}")
+    extensions = []
+
+    os.chdir(os.getcwd())
+    for f in os.listdir():
+        if f.split(".")[-1] in ["py", "git", "gitattributes", "gitignore"]:
+            continue
+        if f.split(".")[-1] not in extensions:
+            extensions.append(f.split(".")[-1])
+            os.mkdir(f.split(".")[-1])
+        os.rename(f, f"{f.split(".")[-1]}\\{f}")
+
+if __name__ == "__main__":
+    main()
